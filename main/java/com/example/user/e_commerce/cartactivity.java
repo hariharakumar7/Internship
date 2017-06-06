0ca36888f828b28b3f7761c904d38c4db1c6f354
hariharakumar7 <hariharakumar7@gmail.com>
2017-06-06 17:53:36 +0530

    Add files via upload

---
+++ main/java/com/example/user/e_commerce/cartactivity.java
@@ -0,0 +1,13 @@
+package com.example.user.e_commerce;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class cartactivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cartactivity);
+    }
+}
